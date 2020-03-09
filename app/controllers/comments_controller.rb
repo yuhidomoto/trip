ddcf7c3f580d9bdc7d788e9b35c1a4bf949a3c21
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-03-09 07:48:43 +0000

    comment機能修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,12 +10,12 @@ class CommentsController < ApplicationController
 
  def destroy
   @comment = Comment.find(params[:mytrip_id])
-  @mytrip = Mytrip.find(comment.mytrip_id)
+  @mytrip = Mytrip.find(@comment.mytrip_id)
 	@comment.destroy
  end
 
 private
 	def comment_params
-	   params.require(:comment).permit(:comment)
+	   params.require(:comment).permit(:body)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :mytrips, dependent: :destroy
-  validates :comment, presence: true
+  has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
