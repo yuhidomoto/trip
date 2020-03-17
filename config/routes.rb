229492bccd2ec28b4b349c985958ec628bd32263
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-03-17 12:31:09 +0000

    namespaceにてadminのルートを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+      sessions: 'admins/sessions',
+      registrations: 'admins/registrations',
+      passwords: 'admins/passwords'
+    }
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	root 'homes#top'
 	get 'homes/about' => 'homes#about'
-	get 'admins/top' => 'admins#top'
   get 'seemore' => 'mytrips#seemore'
 
 	resources :users
@@ -13,6 +17,12 @@ Rails.application.routes.draw do
   resources :mytrips do
   resource :favorites, only: [:create, :destroy]
   resource :comments, only: [:create, :destroy]
-
   end
+
+  namespace :admins do
+    resources :users, only: [:index,:show,:edit,:update]
+    resources :mytrips, only: [:index,:show,:edit,:update,:destroy]
+    get 'top' => 'homes#top'
+  end
+
 end
