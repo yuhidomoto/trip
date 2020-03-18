7b4e9543b4da4f382aaa390d1c9690276e2abf89
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-03-18 03:47:43 +0000

    admins/users/editページ、機能を作成

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,2 +1,28 @@
 class Admins::UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to admins_users_path, notice: "会員情報の変更が完了しました。"
+  	else
+		 render "edit"
+		end
+	end
+
+	def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :gender, :age, :email)
+  end
 end
