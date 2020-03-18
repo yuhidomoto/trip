22a6057ea9dc7873c32737bf4e63c41376046e8b
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-03-18 03:56:30 +0000

    adminのsignin,signout後のpath指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,27 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!, except: [:about,:top]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def after_sign_in_path_for(resource)
-    mytrips_path
+    case resource
+    when Admin
+         admins_top_path
+    when User
+         mytrips_path
+    end
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    if resource == :user
+        root_path
+    else
+      new_admin_session_path
+    end
   end
 
 	protected
 	def configure_permitted_parameters
 	  devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:age,:gender])
-
 
   end
 end
