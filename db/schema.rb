abacdd360b7689bb593e549ce5d474ff2eb24d49
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-03-19 23:41:11 +0000

    mytripsのマイグレーションファイルを修正(不要なカラムを削除)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_030814) do
+ActiveRecord::Schema.define(version: 2020_03_19_233704) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -77,8 +77,6 @@ ActiveRecord::Schema.define(version: 2020_03_12_030814) do
     t.text "bad_point"
     t.text "important"
     t.text "address"
-    t.float "latitude"
-    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
