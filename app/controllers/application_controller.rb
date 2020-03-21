a7abe3b10177d982c146b2a9468fe171b9735eb3
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-03-21 08:35:12 +0000

    アクセス制限削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
   def after_sign_in_path_for(resource)
     case resource
