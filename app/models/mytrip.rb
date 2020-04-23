41c76139bbc7a5c57e7550d80ca4c69b8e0960b0
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-04-23 12:48:21 +0000

    mytrip.rbコメントアウト追加

--- app/models/mytrip.rb
+++ app/models/mytrip.rb
@@ -12,8 +12,10 @@ class Mytrip < ApplicationRecord
 	has_many :favorites, dependent: :destroy
   has_many :tags, dependent: :destroy
 
+  # user = current_user
+  # (user)にcurrent_userを代入している
   def favorited_by?(user)
-   favorites.where(user_id: user.id).exists?
+    favorites.where(user_id: user.id).exists?
   end
 
   def self.search(search)
