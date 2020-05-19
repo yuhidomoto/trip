ba156d6aa0983d973e610c4dc19f1bec4c7ea07d
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-05-19 03:46:20 +0000

    inquiry機能追加(user,admin)によるroutesの変更、修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,42 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions',
-    registrations: 'admins/registrations',
-    passwords: 'admins/passwords'
-  }
+
+  # user #
+  # devise
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :admins do
-    resources :users, only: [:index,:show,:edit,:update,:destroy]
-    resources :mytrips, only: [:index,:show,:edit,:update,:destroy]
-    get 'top' => 'homes#top'
-    get 'seemore' => 'mytrips#seemore'
+  resources :users
+  namespace :users do
+    resources :inquiries, only:[:index, :new, :create, :show]
   end
 
-	root 'homes#top'
-	get 'homes/about' => 'homes#about'
+  # 機能
+  root 'homes#top'
+  get 'homes/about' => 'homes#about'
   get 'seemore' => 'mytrips#seemore'
-
-	resources :users
-
   resources :mytrips do
     resource :favorites, only: [:create, :destroy]
     resource :comments, only: [:create, :destroy]
     collection do
       get 'search' => 'searchs#search'
     end
+  end
+
+#----------------------------------------------
+
+# admin #
+
+# devise
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions',
+  registrations: 'admins/registrations',
+  passwords: 'admins/passwords'
+}
+# 機能 #
+  namespace :admins do
+    resources :users, only: [:index,:show,:edit,:update,:destroy]
+    resources :mytrips, only: [:index,:show,:edit,:update,:destroy]
+    resources :inquiries, only:[:index, :edit, :update, :show, :destroy]
+    get 'top' => 'homes#top'
+    get 'seemore' => 'mytrips#seemore'
   end
 end
