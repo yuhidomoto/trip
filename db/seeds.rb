2fd24340d79b45c1575f278381792cab79d7a415
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-05-23 04:37:09 +0000

    seedsファイル内追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(id:1, email: "admin@admin", password: "111111",name:"admin")
